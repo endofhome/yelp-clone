ac9987bd73611ab5e3c0a068938f6f8880944ae8
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-26 22:33:42 +0300

    Added average ratings

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -10,10 +10,16 @@ feature "reviewing" do
   end
 
   scenario "a user can only leave one review per restaurant" do
-    sign_up("test@test.com", "12345678")    
+    sign_up("test@test.com", "12345678")
     create_restaurant("KFC")
     create_review("KFC", "yuck", "1")
     create_review("KFC", "YUM", "5")
     expect(page).to have_content("prohibited this review from being saved")
+  end
+
+  scenario 'displays an average rating for all reviews' do
+    create_review('KFC', 'So so', '3')
+    create_review('KFC', 'Great', '5')
+    expect(page).to have_content('Average rating: 4')
   end
 end
